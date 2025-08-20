e4e8ff9798fdddde118e542ed23c1d7ee003c558
ugjjffu <56184119+ugjjffu@users.noreply.github.com>
2025-08-20 12:30:28 +0800

    Update page.tsx

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,5 +1,11 @@
+import { getAllPostsData, getCategoryStructure, getCategories } from '@/lib/posts';
+
 export default function BlogPage() {
   // 在服务器端获取数据
+  const blogPosts = getAllPostsData();
+  const categoryStructure = getCategoryStructure();
+  const categories = getCategories();
+  
   return (
     <div>123</div>
   );
